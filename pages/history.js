0e77aa72abfdce22db402b5180375ee43631edd1
kevinhes <kevinhes1115@gmail.com>
2023-11-15 12:09:38 +0800

    ipad rwd

--- pages/history.js
+++ pages/history.js
@@ -9,22 +9,22 @@ export default function OverView({ bannerGroup, contentList }) {
   return (
     <div className='page-ani'>
       <HeaderPageElement></HeaderPageElement>
-      <div className="relative md:h-[730px] lg:h-[660px] xl:h-[600px] 2xl:h-[540px] pt-[30px] md:pt-[138px] lg:pt-[120px] 2xl:pt-[80px]">
-        <div className="relative z-10 w-full flex justify-end md:pr-[81px] mx-auto md:mx-0 max-w-[327px] md:max-w-none mb-[30px] md:mb-0">
+      <div className="relative  xl:h-[600px] 2xl:h-[540px] pt-[30px] md:pt-[60px] xl:pt-[138px] 2xl:pt-[80px]">
+        <div className="relative z-10 w-full flex xl:justify-end xl:pr-[81px] mx-auto xl:mx-0 mb-[30px] md:mb-[100px] max-w-[327px] md:max-w-[720px] lg:max-w-[960px] xl:max-w-none xl:mb-0">
           <div>
-            <h3 className='text-[#2E4E4C] md:text-white md:w-[639px] font-opensans
+            <h3 className='text-[#2E4E4C] md:text-black xl:text-white md:w-[639px] font-opensans
             text-[12px] md:text-[20px] leading-[16px] md:leading-[36px] tracking-[2px] font-normal pb-[10px]
             md:pb-[11px] border-b border-[#2E4E4C] md:border-white md:mb-[36px] mb-[24px]'>
               {bannerGroup.title}
             </h3>
-            <p className='text-[#723C3F] md:text-white
+            <p className='text-[#723C3F] md:text-black xl:text-white
             md:w-[665px] font-crimson md:text-[30px] text-[20px] md:leading-[42px] leading-[22px] font-bold' dangerouslySetInnerHTML={{ __html: bannerGroup.content }} />
           </div>
         </div>
-        <div className="hidden md:block absolute w-full h-full top-0 left-0">
+        <div className="hidden xl:block absolute w-full h-full top-0 left-0">
           <Image src={bannerGroup.bg_img.url} width={1524} height={730} className='w-full h-full object-cover object-left-top' alt="" />
         </div>
-        <div className="md:hidden w-full h-full top-0 left-0">
+        <div className="xl:hidden w-full h-full top-0 left-0">
           <Image src={bannerGroup.sm_bg_img.url} width={614} height={421} className='w-full h-full object-cover' alt="" />
         </div>
       </div>
